ce742d63ef671f544b34d30175017f303b6883da
Marvin <mdengomg2@gmail.com>
2024-02-04 17:19:27 -0800

    Add text extraction button

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QFrame,
     QPushButton,
+    QTextEdit,
     QFileDialog,
 )
-
 from PyQt5.QtCore import Qt
-
 from capturer import Capture
+from PIL import Image
+from pytesseract import pytesseract
 
 
 class ScreenRegionSelector(QMainWindow):
 
-    def __init__(self,):
+    def __init__(self):
         super().__init__(None)
         self.m_width = 400
         self.m_height = 500
@@ -42,9 +43,15 @@ class ScreenRegionSelector(QMainWindow):
         self.btn_extract.clicked.connect(self.extract_text)
         self.btn_extract.setVisible(False)
 
+        self.text_edit = QTextEdit()
+        self.text_edit.setMinimumSize(350, 200)
+        self.text_edit.setVisible(False)
+
         lay.addWidget(self.label)
         lay.addWidget(self.btn_capture)
         lay.addWidget(self.btn_save)
+        lay.addWidget(self.btn_extract)
+        lay.addWidget(self.text_edit)
 
         self.setCentralWidget(frame)
 
@@ -52,6 +59,7 @@ class ScreenRegionSelector(QMainWindow):
         self.capturer = Capture(self)
         self.capturer.show()
         self.btn_save.setVisible(True)
+        self.btn_extract.setVisible(True)
 
     def save(self):
         file_name, _ = QFileDialog.getSaveFileName(
@@ -59,12 +67,23 @@ class ScreenRegionSelector(QMainWindow):
         if file_name:
             self.capturer.imgmap.save(file_name)
 
+    def extract_text(self):
+        path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+        image_path = r"test.png"
+
+        img = Image.open(image_path)
+        pytesseract.tesseract_cmd = path_to_tesseract
+        text = pytesseract.image_to_string(img)
+
+        self.text_edit.setText(text[:-1])
+        self.text_edit.setVisible(True)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet("""
     QFrame {
-        background-color: #3f3f3f;
+        background-color: #ffffff;
     }
                       
     QPushButton {
@@ -83,4 +102,4 @@ if __name__ == "__main__":
     """)
     selector = ScreenRegionSelector()
     selector.show()
-    app.exit(app.exec_())
+    sys.exit(app.exec_())
